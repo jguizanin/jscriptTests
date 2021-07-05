ef10dd0ddcbfa54dcaf3be67b5a55b32b0b768ff
jguizanin <80772689+jguizanin@users.noreply.github.com>
2021-07-05 17:13:55 -0300

    Update viewPlusClick.js

--- viewPlusClick.js
+++ viewPlusClick.js
@@ -1,5 +1,5 @@
 function isInViewport(el) {
-    const rect = el.getBoundingClientRect()
+    let rect = el.querySelector('span').getBoundingClientRect()
     return (
         rect.top >= 0 &&
         rect.left >= 0 &&
@@ -7,32 +7,29 @@ function isInViewport(el) {
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     )}
 
-const itemList = document.querySelectorAll('.prod_link')
+const itemList = Array.from(document.querySelectorAll('.prod_link'))
 const alreadyShown = []
-const newPrint = []
+let newPrint = []
 
-document.addEventListener('scroll', function() {
-	let pageType = window.location.href
-	switch (pageType) {
-		case pageType.includes(home):
-			pageType = home
-			break
-		case pageType.includes(category):
-			pageType = category
-			break
-		case pageType.includes(search):
-			pageType = search
-			break
-	}
-	
-	for (let i = 0; i <= itemList.length; i++) {
+let pageType = window.location.href
+if (pageType.includes('home')) pageType = 'home'
+else if (pageType.includes('category')) pageType = 'category'
+else if (pageType.includes('search')) pageType = 'search'
+else pageType = 'unknown page'
+
+document.addEventListener('scroll', function() {	
+	for (let i = 0; i < itemList.length; i++) {
 		if ((isInViewport(itemList[i])) && !(alreadyShown.includes(itemList[i].getAttribute('href')))) {
-			alreadyShown.append(itemList[i].getAttribute('href'))
-			newPrint.append(itemList[i])
+			alreadyShown.push(itemList[i].getAttribute('href'))
+			newPrint.push(itemList[i])
 		}
 	}
-	
-	console.group('View')
-	console.log(pageType, newPrint)
-	console.groupEnd()
+	setTimeout(function() { 
+		if (newPrint.length != 0) {
+			console.group('View')
+			console.log(pageType, newPrint)
+			console.groupEnd()
+		}
+		newPrint = []
+	}, 500)
 })
