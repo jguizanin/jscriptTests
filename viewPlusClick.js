9a7c763dae241c2e1fb3fd6bff93f85d95ff9809
jguizanin <80772689+jguizanin@users.noreply.github.com>
2021-07-06 11:00:00 -0300

    Update viewPlusClick.js

--- viewPlusClick.js
+++ viewPlusClick.js
@@ -1,11 +1,11 @@
 function isInViewport(el) {
-	let rect = el.querySelector('span').getBoundingClientRect()
-	return (
-	rect.top >= 0 &&
-	rect.left >= 0 &&
-	rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-	rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-)}
+    let rect = el.querySelector('span').getBoundingClientRect()
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    )}
 
 const itemList = Array.from(document.querySelectorAll('.prod_link'))
 const alreadyShown = []
@@ -36,9 +36,14 @@ document.addEventListener('scroll', function() {
 
 for (let j = 0; j < itemList.length; j++) {
 	itemList[j].querySelector('span').addEventListener('click', function() {
-		let item = itemList[j]
 		console.group('Click')
-		console.log(pageType, item)
+		console.log(pageType, [{
+			URL_prod: itemList[j].getAttribute('href'),
+			URL_img: itemList[j].querySelector('img').src,
+			ID: itemList[j].getAttribute('href').split('=')[1],
+			Nome: itemList[j].querySelector('h2').textContent,
+			Preço: itemList[j].querySelector('h3').textContent.split('R$ ')[1].replaceAll('.', '').replace(',', '.'),
+		}])
 		console.groupEnd()
 	})
 }
