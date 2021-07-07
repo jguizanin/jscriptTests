12225e04f011a239fc224d52c89db0cec5f3e8a2
jguizanin <80772689+jguizanin@users.noreply.github.com>
2021-07-07 16:37:35 -0300

    Update viewPlusClick.js

--- viewPlusClick.js
+++ viewPlusClick.js
@@ -1,44 +1,22 @@
-window.addEventListener('load', function() {
+if (document.readyState == 'complete') activateTracker()
+else window.addEventListener('load', activateTracker)
+
+function activateTracker() {
 	let newPrint = []
-	const alreadyShown = []
-	const itemList = Array.from(document.querySelectorAll('.prod_link'))
-		
-	let pageType = window.location.href
-	if (pageType.includes('home')) pageType = 'home'
-	else if (pageType.includes('category')) pageType = 'category'
-	else if (pageType.includes('search')) pageType = 'search'
-	else pageType = 'unknown page'
-		
+	const alreadyShown = []	
+	const pageType = whichPage()
+	const itemList = document.querySelectorAll('.prod_link')
+
 	for (let i = 0; i < itemList.length; i++) {
 		if ((isInViewport(itemList[i])) && !(alreadyShown.includes(itemList[i].getAttribute('href')))) {
 			alreadyShown.push(itemList[i].getAttribute('href'))
 			newPrint.push(itemList[i])
 		}
 	}
-	if (newPrint.length != 0) {
-		console.group('View')
-		console.log(pageType)
-		for (let k = 0; k < newPrint.length; k++) {
-			console.log([{
-				URL_prod: newPrint[k].getAttribute('href'),
-				URL_img: newPrint[k].querySelector('img').src,
-				ID: newPrint[k].getAttribute('href').split('=')[1],
-				Nome: newPrint[k].querySelector('h2').textContent,
-				Preço: newPrint[k].querySelector('h3').textContent.split('R$ ')[1].replaceAll('.', '').replace(',', '.'),
-			}])
-		}
-		console.groupEnd()
-	}
+
+	if (newPrint.length != 0)
+        showResults(newPrint, pageType, 'LoadView')
 	newPrint = []
-	
-	function isInViewport(el) {
-	    let rect = el.querySelector('span').getBoundingClientRect()
-	    return (
-	        rect.top >= 0 &&
-	        rect.left >= 0 &&
-	        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-	        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-	    )}
 	
 	document.addEventListener('scroll', function() {	
 		for (let i = 0; i < itemList.length; i++) {
@@ -48,35 +26,46 @@ window.addEventListener('load', function() {
 			}
 		}
 		setTimeout(function() { 
-			if (newPrint.length != 0) {
-				console.group('View')
-				console.log(pageType)
-				for (let k = 0; k < newPrint.length; k++) {
-					console.log([{
-						URL_prod: newPrint[k].getAttribute('href'),
-						URL_img: newPrint[k].querySelector('img').src,
-						ID: newPrint[k].getAttribute('href').split('=')[1],
-						Nome: newPrint[k].querySelector('h2').textContent,
-						Preço: newPrint[k].querySelector('h3').textContent.split('R$ ')[1].replaceAll('.', '').replace(',', '.'),
-					}])
-				}
-				console.groupEnd()
-			}
+			if (newPrint.length != 0)
+                showResults(newPrint, pageType, 'ScrollView')
 			newPrint = []
 		}, 500)
 	})
 	
-	for (let j = 0; j < itemList.length; j++) {
-		itemList[j].querySelector('span').addEventListener('click', function(item) {
-			console.group('Click')
-			console.log(pageType, [{
-				URL_prod: this.parentElement.getAttribute('href'),
-				URL_img: this.parentElement.querySelector('img').src,
-				ID: this.parentElement.getAttribute('href').split('=')[1],
-				Nome: this.parentElement.querySelector('h2').textContent,
-				Preço: this.parentElement.querySelector('h3').textContent.split('R$ ')[1].replaceAll('.', '').replace(',', '.'),
-			}])
-			console.groupEnd()
-		})
+    for (let j = 0; j < itemList.length; j++) {
+		itemList[j].addEventListener('click', function() {
+		showResults([itemList[j]], pageType, 'Click') })
 	}
-})
+}
+
+function isInViewport(element) {
+	let rect = element.querySelector('span').getBoundingClientRect()
+	return (
+		rect.top >= 0 &&
+		rect.left >= 0 &&
+		rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+	)}
+
+function whichPage() {
+	let pageURL = window.location.href
+	const pageIdentifier = ['home', 'category', 'search']
+	for (let i = 0; i < pageIdentifier.length; i++)
+	    if (pageURL.includes(pageIdentifier[i])) return pageIdentifier[i]
+	return 'unknown page'
+}
+
+function showResults(toPrint, pageType, eventType) {
+	console.group(eventType)
+	console.log(pageType)
+	for (let i = 0; i < toPrint.length; i++) {
+		console.log([{
+			URL_prod: toPrint[i].getAttribute('href'),
+			URL_img: toPrint[i].querySelector('img').src,
+			ID: toPrint[i].getAttribute('href').split('=')[1],
+			Nome: toPrint[i].querySelector('h2').textContent,
+			Preço: toPrint[i].querySelector('h3').textContent.split('R$ ')[1].replaceAll('.', '').replace(',', '.'),
+		}])
+	}
+	console.groupEnd()
+}
